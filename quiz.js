62dc362f4e0ecddb145b44f0b7207361406bebd8
MirzaAyon <ayonmirza98@gmail.com>
2022-06-29 12:09:00 +0600

    some interview ques added

---
+++ quiz.js
@@ -0,0 +1,31 @@
+//2
+const data = { result: [{ userName: { title: 'Mr.', name: "sakib khan" } }] }
+ans: data.result[0].userName.name
+
+//3
+const p = [1, 2, 3]; const q = p.__ ? __(n => Math.pow(n, 3));
+ans: map
+//6 
+What is textContent in javascript ?
+    ans : All the text contents of a node and all its descendants
+    //7
+    Which one is not a data type in JSON ?
+    ans : undefined
+    //13 - eta bhalo kore dekhbo
+    //14 - Which one is the correct way to comment in JSON?
+    //json doesn't support any comment
+    //16 
+    < div id = "p" >
+        <p> hahahahahahaha</p>
+</div >
+
+    <script>
+        const p= document.getElementById("p")
+    </script>
+
+
+ How will you clear the contents from the div ?
+
+    ans : p.innertext = "";
+ans: p.innerHTML = "";
+both
